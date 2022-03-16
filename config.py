463f2d791479e3493110b9b06e13f315b33ff641
Don <96370825+SflixBots@users.noreply.github.com>
2022-03-16 12:37:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ from os import environ
 
 class Config(object):
                    BOT_TOKEN = environ['BOT_TOKEN']
-                   API_ID = int(environ['API_ID'])
+                   APP_ID = int(environ['API_ID'])
                    API_HASH = environ['API_HASH']
